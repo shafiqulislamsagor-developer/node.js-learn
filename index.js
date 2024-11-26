fc74725d5315c193037ab39fb4bc0001666584e6
shafiqulislamsagor-developer <shafiqul.islam.sagor.developer@gmail.com>
2024-11-26 16:18:11 +0600

    server create row node js

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
-const _ = require("lodash");
+/*
+ * Title: Uptime Monitoring Application Version
+ * Description: A RESTFul API to monitor up or down time of user defined links
+ * Author: Sumit Saha (practice : Shafiqul Islam Sagor)
+ * Date: 26/11/2024
+ */
 
-console.log(_.last([1, 2, 3]));
+// Dependencies🧮
+
+const http = require("http");
+
+// app object -module scaffolding
+
+const app = {};
+
+//  configuration
+app.config = {
+  port: 3000,
+};
+
+// create server
+
+app.createServer = () => {
+  const server = http.createServer(app.handleReqRes);
+  server.listen(app.config.port, () => {
+    console.log(`listening on port ${app.config.port}`);
+  });
+};
+
+// handle request response
+
+app.handleReqRes = (req, res) => {
+  res.end("hello world");
+};
+
+app.createServer();
